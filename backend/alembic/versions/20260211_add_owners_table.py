6a4e123eed6df87bcdb567b26f1e54347b76370c
Baptiste-Joubert <baptiste.joubert@pi.esisar.grenoble-inp.fr>
2026-02-11 13:41:35 +0100

    fix/Résolution du probléme de double head alembic

--- backend/alembic/versions/20260211_add_owners_table.py
+++ backend/alembic/versions/20260211_add_owners_table.py
@@ -1,7 +1,7 @@
 """add_owners_table
 
 Revision ID: add_owners_001
-Revises: add_detail_tables_001
+Revises: a1b2c3d4e5f6
 Create Date: 2026-02-11 10:00:00.000000
 
 """
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'add_owners_001'
-down_revision = 'add_detail_tables_001'
+down_revision = 'a1b2c3d4e5f6'
 branch_labels = None
 depends_on = None
 
